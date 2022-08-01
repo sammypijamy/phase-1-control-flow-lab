cd57398cc0dfa66715fe197933d00e4f123cfef6
sammypijamy <samaragransky@outlook.com>
2022-08-01 13:01:52 -0700

    commit tree 771c95606fecfaae07067b706fa1a42d675a938c

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(someValue){
+  let message
+  if (someValue <= 400) {
+    message =  "This one is on me!" }
+  else if (someValue >= 400, someValue <= 2000 ) {
+    message =  "That will be twenty bucks." }
+  else if (someValue >= 2000, someValue <= 2500) {
+    message = "I will gladly take your thirty bucks." }
+  else if (someValue > 2500 ) {
+    message = "No can do."  }
+    return message
+} 
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+      return (city === "NYC") ?  "Ok, sounds good."
+    : "No go." 
+} 
+   
+ 
+function switchOnCharmFromTip(tip){
+let message
+if (tip == "generous") {
+  message = "Thank you so much."
+} 
+else if (tip == "not as generous") {
+  message = "Thank you."
 }
-
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+else message = "Bye."
+  return message
+    }
+  
